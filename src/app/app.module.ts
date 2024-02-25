f163f94ec9b85e0fd0747960deeb26b7e6f096e0
Quynhnb <baoq2015@gmail.com>
2024-02-25 19:31:00 +0100

    add shopappng16

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+
+@NgModule({
+  declarations: [
+
+
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [OrderComponent]
+})
+export class AppModule { }
